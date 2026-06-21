b7133edc214c2c9df62d75be4193c8c5af116c4d
sumit chauhan <sumitchauhan2764@gmail.com>
2026-06-21 12:17:06 +0530

    change in app

--- app.js
+++ app.js
@@ -26,9 +26,9 @@ async function main(){
    await mongoose.connect(MONGO_URL)
 }
 
-app.get("/",(req,res)=>{
-    res.send("home route is working")
-})
+// app.get("/",(req,res)=>{
+//     res.send("home route is working")
+// })
 //index route
 app.get("/listing",async (req , res)=>{
   const lists = await listing.find({});
